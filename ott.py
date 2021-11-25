9e76b1178945ada346dde5cefced08610958e418
56Aang <a89588@alunos.uminho.pt>
2021-11-25 18:04:35 +0000

    servers json

--- ott.py
+++ ott.py
@@ -3,44 +3,41 @@ import threading
 import socket
 
 
-def listener(name):
-    print("Hi I'm " + name)
-    
-    HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-    PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+def listener(name,conn,addr):
 
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((HOST, PORT))
-        s.listen()
-        conn, addr = s.accept()
-        with conn:
-            print('Connected by', addr)
-            while True:
-                data = conn.recv(1024)
-                if not data:
-                    break
-                conn.sendall(data)
-
-def send_hello():   
-    HOST = '127.0.0.1'  # The server's hostname or IP address
-    PORT = 65432        # The port used by the server
-
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((HOST, PORT))
-        s.sendall(b'Hello, world')
-        data = s.recv(1024)
-
-    print('Received', repr(data))
+    print('Connected by', addr)
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            break
+        print(data)
+        conn.sendall(data)
 
 def main():
     
     params = sys.argv[1:]
     if "-s" in params:
-        while True:
-            print("Servidor")
-            x = threading.Thread(target=listener, args=("listener",))
+        HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+        PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind((HOST, PORT))
+        while True:       
+            print("Listening at ")
+            s.listen()
+            conn, addr = s.accept()
+            
+            x = threading.Thread(target=listener, args=("listener",conn,addr))
             x.start()
-            x.join()
-       
+        
+    elif len(params) == 1:
+        serverIp = params[0]
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((serverIp,65432))
+        inp = input("What to say: ")
+        while inp != "":
+            s.sendall(bytes(inp,encoding='utf8'))
+            inp = input("What to say: ")
+        
+        
 
 main()
